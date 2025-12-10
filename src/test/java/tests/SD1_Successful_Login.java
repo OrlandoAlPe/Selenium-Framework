c177d33b71f3da4a0d806f67e06633104fc2b93c
Orlando Álvarez <wiki97ap@gmail.com>
2025-12-10 13:55:35 -0600

    Spelling correction

--- src/test/java/tests/SD1_Successful_Login.java
+++ src/test/java/tests/SD1_Successful_Login.java
@@ -17,18 +17,18 @@ public class SD1_Successful_Login extends BaseTest {
 		LoginPage login = new LoginPage(driver);
 		InventoryPage inventoryPage = new InventoryPage(driver);
 
-		Reporter.log("1. Navigate to: https://www.saucedemo.com");
+		Reporter.log("1. Navigate to: https://www.saucedemo.com.");
 		navigationPage.goHomePage();
 		
-		Reporter.log("2. Write username: 'standard_user'");
-		Reporter.log("3. Write password: 'secret_sauce'");
-		Reporter.log("4. click 'Login' button");
+		Reporter.log("2. Write username: 'standard_user'.");
+		Reporter.log("3. Write password: 'secret_sauce'.");
+		Reporter.log("4. click 'Login' button.");
 		login.standardAccountLogin();
 		
-		Reporter.log("5. Verify Url changed to '/inventory.html'");
+		Reporter.log("5. Verify Url changed to '/inventory.html'.");
 		inventoryPage.validateInventoryPageLoaded();
 		
-		Reporter.log("6. Verify page title is 'Products'");
+		Reporter.log("6. Verify page title is 'Products'.");
 		inventoryPage.verifyInventoryPageTitle();
 	}
 }
